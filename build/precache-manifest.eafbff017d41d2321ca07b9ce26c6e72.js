036d6b63c2efb7f614dc452fcd70c5149f381179
carlosaconde <carlos.andres.conde@gmail.com>
2023-06-24 01:19:34 -0300

    build

---
+++ build/precache-manifest.eafbff017d41d2321ca07b9ce26c6e72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "39ea8de5acf8eaf2735c",
+    "url": "/static/js/main.39ea8de5.chunk.js"
+  },
+  {
+    "revision": "7776f64959433a31fd8c",
+    "url": "/static/js/1.7776f649.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "acb290f37adf0849137a2440551a1948",
+    "url": "/index.html"
+  }
+];
